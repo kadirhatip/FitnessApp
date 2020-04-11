289aa7ad563fdb00e3b19c5d2c6fbdacf2f68dbd
Kadir Hatip <59515899+kadirhatip@users.noreply.github.com>
2020-04-11 14:54:01 +0200

    GTTS implementiert

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import os
 os.system('cls')
 
 import speech_recognition as sr 
+import time 
+import random
+import playsound
+from gtts import gTTS
+
+
 
 rec = sr.Recognizer()
 
@@ -14,22 +20,36 @@ def recordVoice():
         try: 
             vOutput = rec.recognize_google(vInput)
         except sr.UnknownValueError:
-            print('Sorry Boss, I did not get that')
+            adonisSays('Sorry Boss, I did not get that')
         except sr.RequestError:
-            print('Sorry Boss, my speech service seems to be down right now')
+            adonisSays('Sorry Boss, my speech service seems to be down right now')
         return vOutput
 
 def respond(vInput):
     if 'start training' in vInput:
-        print('Ok Boss, starting Fitness App')
+        adonisSays('Ok Boss, starting Fitness App')
         fitnessApp()
+    if 'exit' in vInput:
+        adonisSays('Ok Boss, see you later')
+        exit()
+
+def adonisSays(audioString):
+    tts = gTTS(text=audioString, lang='en')
+    r = random.randint(1,10000000)
+    audioFile = 'audio-' + str(r) + '.mp3'
+    tts.save(audioFile)
+    print(audioString)
+    playsound.playsound(audioFile)
+    os.remove(audioFile)
 
 def fitnessApp():
     pass
 
 
+time.sleep(1)
+adonisSays('Welcome Boss. ')
+while 1:
+    recordedVoice = recordVoice()
+    respond(recordedVoice)
 
-print('How can I help you Boss')
-recordedVoice = recordVoice()
 
-respond(recordedVoice)
