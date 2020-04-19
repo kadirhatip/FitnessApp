100a4d50a19343c1aa28db3f8ba61c59c8baac37
Kadir Hatip <59515899+kadirhatip@users.noreply.github.com>
2020-04-19 23:56:15 +0200

    OK Athena muss noch in setStarten gemacht werden

--- main.py
+++ main.py
@@ -113,33 +113,49 @@ def löscheWorkbook():
     os.remove(workbookTitel)
 def setStarten(_übung, _sets, _reps):
     #öffne workbook
-    wb = load_workbook(filename= workbookTitel)
+    #wb = load_workbook(filename= workbookTitel)
     #öffne sheet 'sheetÜbungen' und 'Trainingsdaten'
     #TODO kann die sheetÜbungen nicht öffnen da sie schon offen sind
-    #sheetÜbungen = wb[sheetÜbungen]
-    #sheetDaten = wb[sheetStatistiken]
-    #Lies bei Set1 die Reps aus und pack sie in neueReps
+    #sheetÜbungen = wb['Uebungen']
+    #sheetDaten = wb['Trainingsstatistiken']
     #Athena sagt wie viele Reps du im Set1 machen sollst und wartet auf die tatsächliche Anzahl an Reps die du ihr sagst
+    for setx in range(1, _sets):
+        athenaSagt("%s. %s Satz. %s Wiederholungen" % (_übung, str(setx) + '.', _reps))
+        athenaSagt("Nachdem du mit der Übung fertig bist, sage bitte wie viele Wiederholungen du geschafft hast. Wecke mich dazu wieder mit: Ok, Athena")
+        time.sleep(1)
+        nutzerStimme = ''
+        while not(nutzerStimme == 'Ok Athena'):
+            nutzerStimme = nutzerSagt()
+
+        athenaSagt("Was sind die Wiederholungen?")
+        reps = nutzerSagt()
+        fortschrittEintragen(reps)
+
+        
+
     #Du sagst es ihr und die neuen Reps werden in den 'Trainingsdaten' eingetragen an die Stelle der Trainingssession(Datum)
     #Athena bestätigt und wiederholt was sie eintragen wird
     #Excel speichern
+
+def fortschrittEintragen(_reps):
     pass
+
+
 
 def starteTraining():
     athenaSagt('Beginnen wir mit dem Training')
     #Workbook und Sheets laden
     wb = load_workbook(filename=workbookTitel)
     sheetÜbungen = wb['Uebungen']
-    sheetDaten = wb['Trainingsstatistiken']
+    #sheetDaten = wb['Trainingsstatistiken']
     #Die Gesamtanzahl an Übungen ablesen und als Variable speichern
     gesamtanzahlÜbungen = sheetÜbungen.max_row-1
     #An die erste Übung gehen, die Anzahl der Sets und Reps abspeichern und setStarten mit den Vars starten
-    for zeile in range(1,gesamtanzahlÜbungen):
-        übung = sheetÜbungen.cell(row=zeile, column=1).value
-        sets = sheetÜbungen.cell(row=zeile, column=2).value
-        reps = sheetÜbungen.cell(row=zeile, column=3).value
+    for stelle in range(2,gesamtanzahlÜbungen):
+        übung = sheetÜbungen.cell(row=stelle, column=1).value
+        sets = sheetÜbungen.cell(row=stelle, column=2).value
+        reps = sheetÜbungen.cell(row=stelle, column=3).value
         setStarten(übung, sets, reps)
-       #fortschrittEintragen()
 
 
     
